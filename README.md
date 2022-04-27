7a97c9db8527599acd53be9c2b2399736acd0b80
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-28 03:01:37 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gmod_nofireinwater
+ 
---
+++ lua/autorun/server/sv_nofireinwater.lua
@@ -0,0 +1,21 @@
+do
+    local ENTITY = FindMetaTable( "Entity" )
+
+    if (ENTITY.SourceIgnite == nil) then
+        ENTITY.SourceIgnite = ENTITY.Ignite
+    end
+
+    function ENTITY:Ignite( ... )
+        if (self:WaterLevel() > 0) then
+            return
+        end
+
+        return self:SourceIgnite( ... )
+    end
+end
+
+hook.Add("OnEntityWaterLevelChanged", "NoFireInWater", function( ent, old, new )
+    if (new > 1) and ent:IsOnFire() then
+        ent:Extinguish()
+    end
+end)
