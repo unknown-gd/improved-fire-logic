ef542194f19d3e486789fa6b6be90aa1bdbee69c
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-28 10:34:16 +0400

    Sound & Upgrade

--- lua/autorun/server/sv_nofireinwater.lua
+++ lua/autorun/server/sv_nofireinwater.lua
@@ -1,4 +1,23 @@
+hook.Add("OnEntityWaterLevelChanged", "No Fire In Water", function( ent, old, new )
+    if (new > 1) and ent:IsOnFire() then
+        ent:Extinguish()
+    end
+end)
+
+local combustible_materials = {
+    [MAT_PLASTIC] = true,
+    [MAT_WOOD] = true,
+    [MAT_FOLIAGE] = true,
+    [MAT_GRASS] = true,
+    [MAT_FLESH] = true,
+    [MAT_BLOODYFLESH] = true,
+    [MAT_ALIENFLESH] = true,
+    [MAT_ANTLION] = true,
+    [MAT_DIRT] = true
+}
+
 do
+
     local ENTITY = FindMetaTable( "Entity" )
 
     if (ENTITY.SourceIgnite == nil) then
@@ -6,16 +25,45 @@ do
     end
 
     function ENTITY:Ignite( ... )
-        if (self:WaterLevel() > 0) then
-            return
-        end
+        if (combustible_materials[ self:GetMaterialType() ] == nil) then return end
+        if (self:WaterLevel() > 0) then return end
+        if self:IsOnFire() then return end
 
         return self:SourceIgnite( ... )
     end
+
+    if (ENTITY.SourceExtinguish == nil) then
+        ENTITY.SourceExtinguish = ENTITY.Extinguish
+    end
+
+    do
+        local sound_path = "player/flame_out.ogg"
+        local math_random = math.random
+        local CHAN_ITEM = CHAN_ITEM
+
+        function ENTITY:Extinguish()
+            self:EmitSound( sound_path, math_random( 55, 65 ), math_random( 60, 180 ), 1, CHAN_ITEM )
+            return self:SourceExtinguish()
+        end
+    end
+
 end
 
-hook.Add("OnEntityWaterLevelChanged", "No Fire In Water", function( ent, old, new )
-    if (new > 1) and ent:IsOnFire() then
-        ent:Extinguish()
-    end
-end)
+do
+
+    local DMG_BURN = DMG_BURN
+    local bit_band = bit.band
+
+    hook.Add("EntityTakeDamage", "No Fire In Water", function( ent, dmg )
+        if (bit_band( dmg:GetDamageType(), DMG_BURN ) == DMG_BURN) then
+            if (combustible_materials[ ent:GetMaterialType() ] == nil) or (ent:WaterLevel() > 1) then
+                if ent:IsOnFire() then
+                    ent:Extinguish()
+                end
+
+                return true
+            end
+        end
+    end)
+
+end
