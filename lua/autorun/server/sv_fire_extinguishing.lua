41562f8799b1b8e3ee35b4ba61acebf74de8314e
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-07-11 01:45:28 +0400

    Update sv_fire_extinguishing.lua

--- lua/autorun/server/sv_fire_extinguishing.lua
+++ lua/autorun/server/sv_fire_extinguishing.lua
@@ -1,3 +1,12 @@
+--[[
+
+    Title: Fire Extinguishing++
+    Workshop: https://steamcommunity.com/sharedfiles/filedetails/?id=2805142659
+    GitHub: https://github.com/PrikolMen/gmod_fire_extinguishing
+    Author: https://steamcommunity.com/id/PrikolMen/
+
+--]]
+
 local addon_name = "Fire Extinguishing"
 
 do
