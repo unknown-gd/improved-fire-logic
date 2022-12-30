d298ab6ac7bc4fa8acf5a66658ffd5d50a12aa20
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-12-30 08:23:07 +0400

    Update sv_fire_extinguishing.lua

--- lua/autorun/server/sv_fire_extinguishing.lua
+++ lua/autorun/server/sv_fire_extinguishing.lua
@@ -1,6 +1,6 @@
 --[[
 
-    Title:Finally a normal fire behavior!
+    Title: Finally a normal fire behavior!
     Workshop: https://steamcommunity.com/sharedfiles/filedetails/?id=2805142659
     GitHub: https://github.com/PrikolMen/gmod_fire_extinguishing
     Author: https://steamcommunity.com/id/PrikolMen/
