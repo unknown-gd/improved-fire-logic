d8bca5c7d3ea77a284ae3d194e3d72f44192678d
PrikolMen <olegis019@gmail.com>
2023-01-18 12:21:48 +0400

    Naming

--- lua/autorun/server/sv_improved_fire_logic.lua
+++ lua/autorun/server/sv_improved_fire_logic.lua
@@ -3,7 +3,6 @@
     Title: Improved Fire Logic & API
     Workshop: https://steamcommunity.com/sharedfiles/filedetails/?id=2805142659
     GitHub: https://github.com/PrikolMen/gmod_fire_extinguishing
-    Author: https://steamcommunity.com/id/PrikolMen/
 
 --]]
 
